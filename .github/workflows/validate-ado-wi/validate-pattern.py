9eb7f7ef6c9a648ab7c1c42c28bdf95ad0249fc9
Denys Pilguiev <denys_p@optimove.com>
2024-09-02 12:01:11 +0300

    add script

--- .github/workflows/validate-ado-wi/validate-pattern.py
+++ .github/workflows/validate-ado-wi/validate-pattern.py
@@ -1,23 +1,39 @@
 import re
+import ast
 import argparse
 import sys  # Import the sys module
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Parameters to validate pattern',
+        usage='-checklist [PR_TITLE, PR_BODY]')
+
+    parser.add_argument('-checklist', dest='checklist', type=str,
+                        help='Specify checklist to parse'
+                             'Example: PR_BODY',
+                        required=True)
+    
+    args = parser.parse_args()
+    checklist = ast.literal_eval(args.checklist)
+
+    return (checklist)
 
 def check_for_pattern(strings):
     pattern = r"AB#\d+"
     check_confirmed = [string for string in strings if re.search(pattern, string)]
     return check_confirmed
 
-def main():
-    parser = argparse.ArgumentParser(description="Check for AB# pattern in a list of strings.")
-    parser.add_argument('strings', nargs='+', help='List of strings to check')
-    args = parser.parse_args()
-
-    check_confirmed = check_for_pattern(args.strings)
+def validate_check(check_confirmed):
     if check_confirmed:
-        print("Found check_confirmed:", check_confirmed)
+        print("Found linked working item:", check_confirmed)
     else:
-        print("No check_confirmed found.")
-        sys.exit(1)  # Exit with code 1 if no check_confirmed are found
+        print("No linked working item found.")
+        sys.exit(1)
+
+def main(checklist):
+    check_result = check_for_pattern(checklist)
+    validate_check(check_result)
 
 if __name__ == "__main__":
-    main()
+    _checklist = parse_args()
+    main(checklist=_checklist)
